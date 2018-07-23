f907f38c87a746bc47d5dc10aff6a9c54fac7154
qqpengqq <qwer492915298@126.com>
2018-07-23 11:40:50 +0800

    first commit

---
+++ src/com/clo/tAction/Hello.java
@@ -0,0 +1,27 @@
+package com.clo.tAction;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Hello extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public Hello() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public void service(HttpServletRequest request,HttpServletResponse response) throws ServletException ,IOException{
+		java.io.PrintWriter out = response.getWriter();
+		out.println("<html><boby>Hello too!</boby></html>");
+		out.close();
+	}
+}
+
